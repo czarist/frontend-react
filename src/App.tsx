46e6305cad30898cb0b8d5fa55ef201e4a191edd
Cezar  Trentin <czartrentin@gmail.com>
2021-07-04 10:18:21 -0300

    consumindo api

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,71 @@
-import React from 'react'
-import './App.scss'
+import React from 'react';
+import axios, { CancelTokenSource } from 'axios';
+import logoMoovin from './logo-roda.png';
 
-const App: React.FC = () => {
-	const logoMoovin =
-		'https://cdn.moovin.com.br/project/manager-panel/img/logo-moovin.svg'
-
-	return (
-		<div className='App'>
-			<img alt='Logo da Moovin' src={logoMoovin} />
-		</div>
-	)
+interface IPost {
+  id: number;
+  userId?: number;
+  title: string;
+  body: string;
 }
 
-export default App
+const defaultPosts: IPost[] = [];
+
+const App = () => {
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] =
+    React.useState(defaultPosts);
+
+  const [loading, setLoading]: [boolean, (loading: boolean) => void] =
+    React.useState<boolean>(true);
+
+  const [error, setError]: [string, (error: string) => void] =
+    React.useState('');
+
+  const cancelToken = axios.CancelToken; //create cancel token
+  const [cancelTokenSource, setCancelTokenSource]: [
+    CancelTokenSource,
+    (cancelTokenSource: CancelTokenSource) => void,
+  ] = React.useState(cancelToken.source());
+
+  const handleCancelClick = () => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel('User cancelled operation');
+    }
+  };
+
+  React.useEffect(() => {
+    axios
+      .get<IPost[]>('https://jsonplaceholder.typicode.com/posts', {
+        cancelToken: cancelTokenSource.token,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch((ex) => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="App">
+        <img alt="Logo da Moovin" src={logoMoovin} />
+      </div>
+      <div className="">
+        <ul className="posts">
+          {posts.map((post) => (
+            <li key={post.id}>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default App;
